113335af6a7681a18969db9ea9e85cbbd327047e
beecodeguy <beecodeguy@gmail.com>
2024-09-23 14:09:00 +0545

    initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="overflow-hidden">{children}</main>
       </body>
     </html>
   );
